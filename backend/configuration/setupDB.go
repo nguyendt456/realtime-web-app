172a5c431f2b55ef067b12bf19752a98efbf8d5c
bronzewhale <nguyendt456@gmail.com>
2023-01-22 21:38:42 +0700

    Expose mongo only inside container

--- backend/configuration/setupDB.go
+++ backend/configuration/setupDB.go
@@ -11,7 +11,7 @@ import (
 )
 
 func ConnectDB() *mongo.Client {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://nguyen:nguyen123@localhost:27017/"))
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://nguyen:nguyen123@mongo:27017/"))
 	if err != nil {
 		log.Fatal(err)
 	}
